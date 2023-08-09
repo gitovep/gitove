a7354dfb3da4aa1f3888b18447c2cf8be5f5ff3b
ninthsun91 <ninthsun91@gmail.com>
2023-08-10 02:31:39 +0900

    :recycle: Split commit questions to separate file

--- src/commands/commit/action.ts
+++ src/commands/commit/action.ts
@@ -1,31 +1,10 @@
-import inquirer, { QuestionCollection } from 'inquirer';
+import inquirer from 'inquirer';
 import { commitMessage } from './message';
+import { commitQuestions } from './questions';
 import { is } from 'typia';
-import { CommitMessageInput } from 'types/commit';
-
-type CommitMessageAnswers = Record<CommitMessageInput, string | undefined>;
-
-const questions: QuestionCollection<CommitMessageAnswers> = [
-  {
-    name: CommitMessageInput.TYPE,
-    type: 'input',
-  },
-  {
-    name: CommitMessageInput.SCOPE,
-    type: 'input',
-  },
-  {
-    name: CommitMessageInput.TITLE,
-    type: 'input',
-  },
-  {
-    name: CommitMessageInput.BODY,
-    type: 'input',
-  },
-];
 
 export const commitAction = async () => {
-  const answers: CommitMessageAnswers = await inquirer.prompt(questions);
+  const answers: CommitMessageAnswers = await inquirer.prompt(commitQuestions);
 
   Object.entries(answers).forEach(([key, value]) => {
     if (value && is<CommitMessageInput>(key)) {
--- src/types/commit.ts
+++ src/types/commit.ts
@@ -7,4 +7,6 @@ export const CommitMessageInput = {
 
 declare global {
   type CommitMessageInput = typeof CommitMessageInput[keyof typeof CommitMessageInput];
+
+  type CommitMessageAnswers = Record<CommitMessageInput, string | undefined>;
 }
