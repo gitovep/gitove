b1561a01a22c17ca5595da2f337e7b332c7f64c6
ninthsun91 <ninthsun91@gmail.com>
2023-08-12 22:38:44 +0900

    :recycle: Clean up codes
    - remove unnecessary console log
    - change require to proper import statement
    - move typia to devDependency in lock

--- src/config/alias.ts
+++ src/config/alias.ts
@@ -2,7 +2,6 @@ import { join } from 'node:path';
 import moduleAlias from 'module-alias';
 
 const DIST_PATH = join(__dirname, '..', 'types');
-console.log(DIST_PATH);
 
 moduleAlias.addAliases({
   types: DIST_PATH,
