98d6a2bcbcf661b3ec4da0e2110ceb5a991245c7
codeing999 <109027875+codeing999@users.noreply.github.com>
2023-08-13 14:17:31 +0900

    [commit] modify convert method (#18)
    * modify convert method
    
    * :white_check_mark: Add insertHeader test code

---
+++ src/commands/insertHeader/insertHeader.ts
@@ -0,0 +1,28 @@
+import { DEFAULT_CONFIG } from '../../config/defaultConfig';
+
+// 설정 파일에서 type에 해당하는 code로 변환하기
+const convertCommitType = async (type: string | undefined): Promise<string | undefined> => {
+  return DEFAULT_CONFIG.type.find((t) => t.name === type)?.name;
+};
+
+// 설정 파일에서 scope에 해당하는 code로 변환하기
+const convertCommitScope = async (scope: string | undefined): Promise<string | undefined> => {
+  return DEFAULT_CONFIG.scope.find((s) => s.name === scope)?.name;
+};
+
+const insertHeader = (commitMessage: CommitMessage): string => {
+  // type 변환
+  const convertedType = convertCommitType(commitMessage.type);
+  // scope 변환
+  const convertedScope = convertCommitScope(commitMessage.scope);
+
+  const type = convertedType ? `[${convertedType}]` : '';
+  const scope = convertedScope ? `[${convertedScope}]` : '';
+  const { title } = commitMessage;
+
+  // 변환 된 헤더를 타이틀에 합친 후 타이틀 리턴
+  // 예시 : [type][scope]title
+  return type + scope + title;
+};
+
+export { insertHeader };
--- src/types/commit.ts
+++ src/types/commit.ts
@@ -9,4 +9,12 @@ declare global {
   type CommitMessageInput = typeof CommitMessageInput[keyof typeof CommitMessageInput];
 
   type CommitMessageAnswers = Record<CommitMessageInput, string | undefined>;
+
+  type CommitMessage = {
+    type: string | undefined; // 커밋 종류
+    scope: string | undefined; // 커밋 도메인
+    title: string; // 커밋 제목
+    body: string | undefined; // 커밋 내용
+  };
+
 }
