74104e1fa5b4dcc7a31e13ac59dd62ebceef5d35
ninthsun91 <ninthsun91@gmail.com>
2023-08-13 13:25:16 +0900

    :sparkles: Make scope inquery optional

--- src/commands/commit/questions.ts
+++ src/commands/commit/questions.ts
@@ -1,5 +1,6 @@
 import inquirer, { QuestionCollection } from 'inquirer';
 import inquirerAutocompletePrompt from 'inquirer-autocomplete-prompt';
+import { DEFAULT_CONFIG } from 'src/config/defaultConfig';
 import { CommitMessageInput } from 'src/types/commit';
 import { filterCommitScope, filterCommitType } from 'src/utils/filterHeaders';
 
@@ -19,19 +20,23 @@ export const commitQuestions: QuestionCollection<CommitMessageAnswers> = [
       );
     },
   },
-  {
-    name: CommitMessageInput.SCOPE,
-    message: 'Choose commit scope:',
-    type: 'autocomplete',
-    source: (_: unknown, input: string) => {
-      return Promise.resolve(
-        filterCommitScope(input).map((keyword) => ({
-          name: `${keyword.alias} - ${keyword.description}`,
-          value: keyword.name,
-        })),
-      );
-    },
-  },
+  ...(DEFAULT_CONFIG.scope.length > 0
+    ? [
+      {
+        name: CommitMessageInput.SCOPE,
+        message: 'Choose commit scope:',
+        type: 'autocomplete',
+        source: (_: unknown, input: string) => {
+          return Promise.resolve(
+            filterCommitScope(input).map((keyword) => ({
+              name: `${keyword.alias} - ${keyword.description}`,
+              value: keyword.name,
+            })),
+          );
+        },
+      },
+    ]
+    : []),
   {
     name: CommitMessageInput.TITLE,
     type: 'input',
