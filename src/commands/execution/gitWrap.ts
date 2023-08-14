e3ac38f9c7463af908002ccc31c253a8b0315508
hoduulmu <62408879+hoduulmu@users.noreply.github.com>
2023-08-14 17:38:08 +0900

    [commit] Simplify message creation (#37)
    * [feat] Simplify Message Creation
    
    * [refactor] Change Filename To Camel Case

---
+++ src/commands/execution/gitWrap.ts
@@ -0,0 +1,19 @@
+import executeGitCommand from './command';
+
+const NEW_LINE: string = '\n';
+
+export const add = async (): Promise<void> => {
+  await executeGitCommand('add', '.');
+};
+
+export const push = async (): Promise<void> => {
+  await executeGitCommand('push');
+};
+
+export const commit = async (
+  title:string,
+  bodies: string[],
+) => {
+  const message: string = `-m ${title}${NEW_LINE}${NEW_LINE}${bodies.join(NEW_LINE)}`;
+  await executeGitCommand('commit', message);
+};
