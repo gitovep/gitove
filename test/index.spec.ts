67dad40fc0644f821a86123f6d9bfeef844566e0
ninthsun91 <ninthsun91@gmail.com>
2023-08-17 04:33:49 +0900

    :white_check_mark: Update tests - CommitMessage type changed

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,12 +2,5 @@ import { DEFAULT_CONFIG } from 'src/config/defaultConfig';
 
 // Temporary test to check alias path working
 it('should pass', () => {
-  const M: CommitMessage = {
-    type: 'feat',
-    scope: 'test',
-    title: 'test',
-    body: 'test',
-  };
-  console.log(M);
   expect(DEFAULT_CONFIG).toBeDefined();
 });
